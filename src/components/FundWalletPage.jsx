6b4d2ed6f49efa93e0bbf6742a5e867f80ce44df
Olanike Olatunji <95747872+olanikegloria@users.noreply.github.com>
2025-03-08 22:07:01 +0100

    these are the initil styles and structure of the app (#5)

---
+++ src/components/FundWalletPage.jsx
@@ -0,0 +1,12 @@
+const FundWalletPage = () => {
+    return (
+      <div className="page-container">
+        <h1>Fund Wallet</h1>
+        <p>This is the Fund Wallet page.</p>
+      </div>
+    )
+  }
+  
+  export default FundWalletPage
+  
+  
