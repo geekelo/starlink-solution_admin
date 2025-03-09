3deba68dd4b13fb5c6a037f3442407969802cc68
nicojoy-gif <orjionyinyechi080@gmail.com>
2025-03-09 22:11:20 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import "./styles/App.css"
-import Login from "./components/Login"
-import SignUp from "./components/SignUp"
-import Sidebar from "./components/Sidebar"
-import RequestPage from "./components/RequestPage"
-import FundWalletPage from "./components/FundWalletPage"
-import WalletPage from "./components/WalletPage"
-import TransactionHistoryPage from "./components/TransactionHistoryPage"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles/App.css';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 import AdminPanel from './components/Admin';
 import UserDetails from './components/Userdetail';
 
 function App() {
-  // Check if user is logged in (this is a placeholder, implement actual auth logic)
-  const isLoggedIn = true // Set to true for development, implement actual auth check
-
   return (
     <Router>
-      <div className="app">
-        {isLoggedIn ? (
-          <div className="dashboard-layout">
-            <Sidebar />
-            <div className="main-content">
-              <Routes>
-                <Route path="/" element={<RequestPage />} />
-                <Route path="/fund-wallet" element={<FundWalletPage />} />
-                <Route path="/wallet" element={<WalletPage />} />
-                <Route path="/transactions" element={<TransactionHistoryPage />} />
-              </Routes>
-            </div>
-          </div>
-        ) : (
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="*" element={<Login />} />
-          </Routes>
-        )}
-      </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<AdminPanel />} /> 
+        <Route path="/user/:id" element={<UserDetails />} />
+
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
-
+export default App;
