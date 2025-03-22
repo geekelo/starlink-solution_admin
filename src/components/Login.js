b51044ed4786ea61a8ddebd04fa6ac05467beaef
Orji Onyinyechi <orjionyinyechi080@gmail.com>
2025-03-22 19:37:20 +0100

    Login update (#13)
    * adjusted login api

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
 
     try {
       const axiosInstance = createAxiosInstance();
-      const response = await axiosInstance.post('/api/v1/admin/login', {
+      const response = await axiosInstance.post('/api/v1/login', {
         starlink_user: {
           email,
           password,
