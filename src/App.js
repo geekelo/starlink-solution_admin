bfdceb7a97780a9d1601f33dff76fe469c2d17ba
geekelo <efueliht12@gmail.com>
2025-03-23 21:09:41 +0100

    Display navbar for protected routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import "./styles/App.css";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
@@ -7,6 +7,16 @@ import AdminPanel from "./components/Admin";
 import FundingPage from "./components/funding/ManageFunding";
 import UserDetails from "./components/user/Userdetail";
 import RenewalPage from "./components/renewal/ManageRenewal";
+import AdminMobileNav from "./components/nav/Nav";
+
+const AdminLayout = () => {
+  return (
+    <div>
+      <AdminMobileNav />
+      <Outlet /> {/* This will render the respective protected page */}
+    </div>
+  );
+};
 
 function App() {
   const token = localStorage.getItem("candra"); // Check if user is logged in
@@ -14,7 +24,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Redirect to login if no token */}
+        {/* Public Routes */}
         {!token ? (
           <>
             <Route path="/login" element={<Login />} />
@@ -23,15 +33,16 @@ function App() {
           </>
         ) : (
           <>
-            {/* Redirect logged-in users away from the login page */}
+            {/* Redirect logged-in users away from auth pages */}
             <Route path="/login" element={<Navigate to="/" replace />} />
             <Route path="/signup" element={<Navigate to="/" replace />} />
 
-            {/* Protected Routes */}
-            <Route path="/" element={<AdminPanel />} />
-            <Route path="/user/:id" element={<UserDetails />} />
-            <Route path="/funding" element={<FundingPage />} />
-            <Route path="/renewals" element={<RenewalPage />} />
+            {/* Protected Routes with AdminMobileNav */}
+            <Route element={<AdminLayout />}>
+              <Route path="/user/:id" element={<UserDetails />} />
+              <Route path="/funding" element={<FundingPage />} />
+              <Route path="/renewals" element={<RenewalPage />} />
+            </Route>
 
             {/* Redirect unknown routes to home */}
             <Route path="*" element={<Navigate to="/" replace />} />
