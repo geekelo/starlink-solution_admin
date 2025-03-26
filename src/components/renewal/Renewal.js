f9dc3752234f684ab753a312b9c501fbbceaf691
nicojoy-gif <orjionyinyechi080@gmail.com>
2025-03-26 15:36:41 +0100

    change the current fallback text on status from pending to receipt on renewal page

--- src/components/renewal/Renewal.js
+++ src/components/renewal/Renewal.js
@@ -52,6 +52,7 @@ const Renewal = ({ transaction, openModal }) => {
         setShowDropdown(false);
       }
     };
+    console.log(transaction)
 
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
@@ -118,7 +119,7 @@ const Renewal = ({ transaction, openModal }) => {
         <div className="kit-info-text">
   <strong>Status:</strong> 
   <span className={`status-badge ${transaction?.status?.toLowerCase()}`}>
-    {transaction?.status || "N/A"}
+    {transaction?.status || "receipt"}
   </span>
 </div>
 
