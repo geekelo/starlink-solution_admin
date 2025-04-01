a9fd7512bb06da7d62b302baeda013b8b49bf1a9
Callylove <106864121+Callylove@users.noreply.github.com>
2025-04-01 13:16:45 +0100

    Path issued fixed (#87)

--- src/components/AppLayout/layout.js
+++ src/components/AppLayout/layout.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import MobileDrawer from "../nav/MobileDrawer";
 import { AdminSidebar } from "../nav/Sidebar";
 import { Outlet } from "react-router-dom";
-import { MobileHeader } from "../nav/Header";
+import { MobileHeader } from "../nav/MobileHeader";
 
 const AdminLayout = () => {
     const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
