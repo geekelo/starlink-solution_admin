1fb713f361c5bd1d23a3a085177d6b2c45a6bff0
nicojoy-gif <orjionyinyechi080@gmail.com>
2025-04-09 20:52:15 +0100

    removed id and added transaction id

--- src/components/request/FundCard.js
+++ src/components/request/FundCard.js
@@ -100,7 +100,7 @@ const FundingCard = ({ item }) => {
         <div className="info-item">
           <IdCard size={16} />
           <p>
-            <strong>Id:</strong> {item.id}
+            <strong>Transaction:</strong> {item.Transaction_id}
           </p>
         </div>
         <div className="info-item">
