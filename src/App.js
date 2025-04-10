9d4f584576594934d5f255a5e3ce4b0db5998016
nicojoy-gif <orjionyinyechi080@gmail.com>
2025-04-10 15:44:35 +0100

    updated message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React  from "react";
 
-
+import "react-toastify/dist/ReactToastify.css";
 import AppRouter from "./components/Routes/Router";
 import './styles/form.css';
 import './styles/shadcn.css';
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
 
   return (
+    <>
    <AppRouter/>
+   <ToastContainer position="top-right" autoClose={3000} />
+   </>
   );
 }
 
